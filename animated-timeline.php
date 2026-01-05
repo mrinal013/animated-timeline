debb40d48029aa4e5711f68e9c998cf1763d4822
mrinal013 <mrinalhaque99@gmail.com>
2026-01-05 22:38:16 +0600

    html modifiction, scriptsheet, stylesheet add

--- animated-timeline.php
+++ animated-timeline.php
@@ -13,3 +13,74 @@
 if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly.
 }
+
+/**
+ * Modify the core Group block.
+*
+* @param string $block_content The block content about to be rendered.
+*
+* @return string The maybe modified block content.
+*/
+
+add_filter( 'render_block_core/group', 'animated_timeline_filter_group_content' );
+function animated_timeline_filter_group_content( $block_content ) {
+	$processor = new WP_HTML_Tag_Processor( $block_content );
+	$counter   = 0;
+
+	// Check for the presence of the animated-timeline class.
+	if ( ! $processor->next_tag( array( 'class_name' => 'animated-timeline' ) ) ) {
+		return $block_content;
+	}
+
+	// Loop through each child block with the class name 'wp-block-column'.
+	while ( $processor->next_tag( array( 'class_name' => 'wp-block-column' ) ) ) {
+		$processor->add_class( 'animated__item' );
+		++$counter;
+
+		switch ( $counter ) {
+			case 1:
+				$processor->add_class( 'animated__item--first' );
+				break;
+			case 2:
+				$processor->add_class( 'animated__item--line' );
+				break;
+			case 3:
+				$processor->add_class( 'animated__item--last' );
+				break;
+		}
+	}
+
+	$block_content = $processor->get_updated_html();
+
+	// Enqueue the custom script and style.
+	wp_enqueue_script( 'animated-timeline-script' );
+	wp_enqueue_style( 'animated-timeline-style' );
+
+	// Return the maybe modified block content.
+	return $block_content;
+}
+
+/**
+ * Registers the custom script and style for the timeline plugin.
+ */
+add_action( 'wp_enqueue_scripts', 'animated_timeline_register_scripts' );
+
+function animated_timeline_register_scripts() {
+	// Register the custom script to be used later.
+	wp_register_script(
+		'animated-timeline-script',
+		plugin_dir_url( __FILE__ ) . '/assets/scripts/core-blocks/group--animated-timeline.js',
+		array(),
+		'1.0.0',
+		true
+	);
+
+    // Register the custom style to be used later.
+	wp_register_script(
+		'animated-timeline-style',
+		plugin_dir_url( __FILE__ ) . '/assets/styles/core-blocks/group--animated-timeline.css',
+		array(),
+		'1.0.0',
+		true
+	);
+}
