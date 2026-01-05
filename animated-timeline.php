81780b20fdecda62a3aa8eda5d7cfc72f65d18f7
mrinal013 <mrinalhaque99@gmail.com>
2026-01-05 21:36:05 +0600

    Animated timeline init

---
+++ animated-timeline.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name: Animated Timeline
+ * Description: Extends the Group block for an animating timeline effect.
+ * Requires at least: 6.5
+ * Requires PHP: 7.4
+ * Version: 1.0.0
+ * License: GPL-2.0-or-later
+ * License URI: https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain: animated-timeline
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+}
